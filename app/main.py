72319d339daf458c4d8594603c5a477d5a892508
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-15 14:22:58 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -20,10 +20,8 @@ def main():
         file_contents = file.read()
 
     # Uncomment this block to pass the first stage
-    # if file_contents:
-    #     raise NotImplementedError("Scanner not implemented")
-    # else:
-    #     print("EOF  null") # Placeholder, remove this line when implementing the scanner
+     if file_contents:
+         raise NotImplementedError("Scanner not implemented")
 
 
 if __name__ == "__main__":
