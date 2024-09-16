eadbc6fd13234871d6e10056c8707e9a09012eca
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-16 18:32:00 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,14 +7,14 @@ def main():
 
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
-        exit(0)
+        exit(1)
 
     command = sys.argv[1]
     filename = sys.argv[2]
 
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
-        exit(0)
+        exit(1)
 
     with open(filename) as file:
         file_contents = file.read()
