a15ad1ffc01d6a8545089c83efa41f58c37ab6d6
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-16 22:34:54 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 import sys
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
@@ -12,36 +10,38 @@ def main():
         exit(1)
     with open(filename) as file:
         file_contents = file.read()
-    # Uncomment this block to pass the first stage
-    if file_contents:
-        # print("EOF  null")
-        for line in file_contents:
-            for char in line:
-                match char:
-                    case "(":
-                        token = "LEFT_PAREN"
-                    case ")":
-                        token = "RIGHT_PAREN"
-                    case "{":
-                        token = "LEFT_BRACE"
-                    case "}":
-                        token = "RIGHT_BRACE"
-                    case ",":
-                        token = "COMMA"
-                    case ".":
-                        token = "DOT"
-                    case "-":
-                        token = "MINUS"
-                    case "+":
-                        token = "PLUS"
-                    case ";":
-                        token = "SEMICOLON"
-                    case "*":
-                        token = "STAR"
-                    case _:
-                        continue
-                        # token = ""
-                print(f"{token} {char} null")
-    print("EOF  null")
+        error=False
+        for c in file_contents:
+            if c == "(":
+                print("LEFT_PAREN ( null")
+            elif c == ")":
+                print("RIGHT_PAREN ) null")
+            elif c == "{":
+                print("LEFT_BRACE { null")
+            elif c == "}":
+                print("RIGHT_BRACE } null")
+            elif c == ",":
+                print("COMMA , null")
+            elif c == ".":
+                print("DOT . null")
+            elif c == "-":
+                print("MINUS - null")
+            elif c == "+":
+                print("PLUS + null")
+            elif c == ";":
+                print("SEMI-COLON ; null")
+            elif c == "*":
+                print("STAR * null")
+            elif c == "/":
+                print("SLASH / null")
+            else:
+                error=True
+                line_number=file_contents.count("\n",0,file_contents.find(token))+1
+                print("[line %s] Error: Unexpected charecter: %s"%(line_number,c),file=sys.stderr,)                
+        print("EOF  null")
+        if error==True:
+            exit(65)
+        else:
+            exit(0)
 if __name__ == "__main__":
     main()
