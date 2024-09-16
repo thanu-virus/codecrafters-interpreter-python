8f5d54ba11c63ad19d1a5abf160eb4014205c730
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-16 18:37:21 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,34 +1,26 @@
 import sys
-
-
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
-
     command = sys.argv[1]
     filename = sys.argv[2]
-
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
-
     with open(filename) as file:
         file_contents = file.read()
-
-    # Uncomment this block to pass the first stage
     if file_contents:
-         raise NotImplementedError("Scanner not implemented")
-    else: # Placeholder, remove this line when implementing the scanner
-
-        for c in file_contents:
-            if c =="(":
-                print("LEFT_PAREN ( null")
-            if c==")":
-                print("RIGHT_PAREN ) null")
-        print("EOF null")
+        raise NotImplementedError("Scanner not implemented")
+    else:
+        print(
+            "EOF  null"
+        )  # Placeholder, remove this line when implementing the scanner
+    for c in file_contents:
+        if c == "(":
+            print("LEFT_PAREN ( null")
+        if c == ")":
+            print("RIGHT_PAREN ) null")
+    print("EOF  null")
 if __name__ == "__main__":
     main()
