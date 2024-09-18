555c28a3a75b80b7fa369efa04bde1df84489a5d
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-18 18:58:45 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -89,7 +89,7 @@ def main():
                     snum+=str(ch)
                     ptr+=1
                     ch=file_contents[ptr]
-                    if ptr < length and ch.isnumeric() !=True:
+                    if ptr < length+1 and ch.isnumeric() !=True:
                         break
 
             else:
