ead335a7108bf4203d0659d370c377d6f6b04db5
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-18 13:26:40 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -86,7 +86,6 @@ def main():
                     ch_name = "SLASH"
             elif ch == " " or ch == "\r" or ch == "\t":
                 line_no+=1
-                pass
                 ptr+=1
                 continue
             else:
